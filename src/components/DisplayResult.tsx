6be41b0b390f914996cf30f7b4e10804a4d509f6
KrisztinaBrczan <brczan.kriszti@gmail.com>
2024-08-04 21:40:21 +0200

    implement interval to display number of primes found

--- src/components/DisplayResult.tsx
+++ src/components/DisplayResult.tsx
@@ -1,13 +1,55 @@
-export default function DisplayResult() {
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+const Textarea = styled.textarea`
+  width: 50%;
+`;
+
+interface receivedProps {
+  isPrime: boolean | null;
+  setIsPrime: React.Dispatch<React.SetStateAction<boolean | null>>;
+  primes: number[];
+  setPrimes: React.Dispatch<React.SetStateAction<number[]>>;
+}
+
+const DisplayResult: React.FC<receivedProps> = ({
+  isPrime,
+  setIsPrime,
+  primes,
+  setPrimes,
+}) => {
+  const [count, setCount] = useState<number | null>(null);
+
+  useEffect(() => {
+    let interval: number;
+    let count: number = 0;
+    if (count <= primes.length) {
+      interval = setInterval(() => {
+        if (count === primes.length) {
+          clearInterval(interval);
+        }
+        setCount(count++);
+      }, 50);
+    }
+    return () => clearInterval(interval);
+  }, [primes]);
+
   return (
     <>
       <div>
-        <span>Number of primes found: 0000</span>
+        <span>
+          Number of primes found:{" "}
+          <span style={{ display: count === 0 ? "none" : "inline" }}>
+            {count}
+          </span>
+        </span>
         <div>
-          <textarea></textarea>
+          <Textarea></Textarea>
         </div>
         <button>Clear</button>
       </div>
     </>
   );
-}
+};
+
+export default DisplayResult;
