6db95bd3c916e24531d874f4e4519f4c0228faeb
KrisztinaBrczan <brczan.kriszti@gmail.com>
2024-08-04 17:11:10 +0200

    remove MainContainer component and add its content to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import Header from "./components/Header";
-import MainContainer from "./components/MainContainer";
+import DisplayResult from "./components/DisplayResult";
+import InputContainer from "./components/InputContainer";
+import styled from "styled-components";
+
+const Container = styled.div`
+  border: 1px solid black;
+  border-radius: 2em;
+  padding: 1em;
+  margin: auto;
+  box-shadow: 5px 5px 10px grey;
+`;
 
 export default function App() {
   return (
     <>
       <Header />
-      <MainContainer />
+      <Container>
+        <InputContainer />
+        <DisplayResult />
+      </Container>
     </>
   );
 }
