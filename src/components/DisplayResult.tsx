f7b7ca96a61e9aca65525e4d09bbd707a3ee5317
KrisztinaBrczan <brczan.kriszti@gmail.com>
2024-08-04 16:59:17 +0200

    create basic display results component

---
+++ src/components/DisplayResult.tsx
@@ -0,0 +1,13 @@
+export default function DisplayResult() {
+  return (
+    <>
+      <div>
+        <span>Number of primes found: 0000</span>
+        <div>
+          <textarea></textarea>
+        </div>
+        <button>Clear</button>
+      </div>
+    </>
+  );
+}
